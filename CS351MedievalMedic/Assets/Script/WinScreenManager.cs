799d0451fea8c794efd07e44249755bb677a5a73
LennyOzaeta <firetag1187@gmail.com>
2025-10-13 22:34:50 -0500

    Added some "flair"

--- CS351MedievalMedic/Assets/Script/WinScreenManager.cs
+++ CS351MedievalMedic/Assets/Script/WinScreenManager.cs
@@ -3,15 +3,40 @@
     Assignment: Team Project (Medieval Medic)
     Description: Manages the win screen
     Initially Created: Sunday, 10/12/25
+        Modified: Monday, 10/13/25
 */
 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement; // Needed to manage scene
+using TMPro; // Needed to use TMP_Text
 
 public class WinScreenManager : MonoBehaviour
 {
+    private int index;
+    // Following are all set in Inspector
+    public TMP_Text textbox;
+    public string[] sentences;
+    public float typingSpeed;
+
+    // Start dialog coroutine
+    private void OnEnable()
+    {
+        StartCoroutine(Type());
+    }
+
+    // Coroutine (types one letter at a time)
+    IEnumerator Type()
+    {
+        textbox.text = ""; // Start textbox as empty
+        foreach (char letter in sentences[index]) // Loop through each letter in message
+        {
+            textbox.text += letter;
+            yield return new WaitForSeconds(typingSpeed); // Wait before continuing executing coroutine
+        }
+    }
+
     // This function is called when the "Main Menu" button is clicked
     public void RestartGame()
     {
